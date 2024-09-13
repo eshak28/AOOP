d7c88ba5343f4ebd70b083de163d9c21a57d53cb
eshak28 <165585456+eshak28@users.noreply.github.com>
2024-09-13 13:56:49 +0530

    Delete WEEK 7 directory

--- WEEK 7/InLab/MinMax/src/minmax/ValueFInder.java
+++
@@ -1,6 +0,0 @@
-package minmax;
-
-public interface ValueFInder <T extends Comparable<T>> {
-	   T findMax(T[] array);
-	    T findMin(T[] array);
-	}
--- WEEK 7/InLab/MinMax/src/minmax/ValueFinder1.java
+++
@@ -1,6 +0,0 @@
-package minmax;
-
-public interface ValueFinder1<T> {
-	 T findMax(T[] array);
-	    T findMin(T[] array);
-}
